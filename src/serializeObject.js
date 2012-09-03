b4f2398ce0ef154ed1c1058cf259f41dafef916c
Victor Arias <arias.sp@gmail.com>
2012-09-03 12:02:49 -0300

    adding a feature to serialize only requested fields if requested

--- src/serializeObject.js
+++ src/serializeObject.js
@@ -6,7 +6,7 @@
  * Licensed under the MIT, GPL licenses.
  */
 (function( $ ){
-  $.fn.serializeObject = function() {
+  $.fn.serializeObject = function(names) {
     if ( this.length < 1) { 
       return false; 
     }
@@ -14,14 +14,15 @@
     var $el = this;
     var data = {};
     var lookup = data; //current reference of data
+    names = names || null;
 
     $el.find( ':input[type!="checkbox"][type!="radio"], input:checked' ).each( function() {
       // data[a][b] becomes [ data, a, b ]
       var named = this.name.replace(/\[([^\]]+)?\]/g, ',$1').split(',');
       var cap = named.length - 1;
-
+       
       // Ensure that only elements with valid `name` properties will be serialized
-      if ( named[ 0 ] ) {
+      if ( named[ 0 ] && (names == null || names[named[0]]) ) {
         for ( var i = 0; i < cap; i++ ) {
             // move down the tree - create objects or array if necessary
             lookup = lookup[ named[i] ] = lookup[ named[i] ] ||
